241d228007fdf8a3d51193f9c5d12baf3f45e8af
Amar Bodonguud <tomreddle70@gmail.com>
2025-08-21 12:37:51 +0800

    thursday

--- src/app/todo/page.js
+++
@@ -1,5 +0,0 @@
-const Todo = () => {
-  return <div className="font-extrabold text-4xl">hello</div>;
-};
-
-export default Todo;
---
+++ src/app/todo/page.jsx
@@ -0,0 +1,23 @@
+const Todo = () => {
+  return (
+    <div>
+      <div className="mt-[140px] flex justify-center">
+        <div className="w-[377px] py-6 px-4 shadow-xl">
+          <h1 className="text-center font-extrabold text-2xl">To-Do-list</h1>{" "}
+          <div className="flex gap-1.5">
+            <input
+              className="w-70 h-10 py-2 px-4 flex items-start rounded-md border-solid border-[1px] border-[#E4E4E7]"
+              type="text"
+              placeholder="Add new task.."
+            ></input>{" "}
+            <button className="bg-[#3C82F6] rounded-md py-2 px-4 text-white ">
+              Add
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
